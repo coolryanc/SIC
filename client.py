fd936e1621c3a3123cfd9b4f67e2860f1f4ec683
TeYanWu <killerw74@gmail.com>
2016-10-13 16:45:26 +0800

    host and client

--- client.py
+++ client.py
@@ -1,21 +1,62 @@
-import socket
-import sys
+import asyncore, socket
+import threading
 
-HOST, PORT = "10.0.1.21", 4001
-data = " ".join(sys.argv[1:])
+class SocketClient(asyncore.dispatcher):
 
-# Create a socket (SOCK_STREAM means a TCP socket)
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    def __init__(self, host, port):
+        asyncore.dispatcher.__init__(self)
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connect( (host, port) )
+        # self.buffer = 'GET %s HTTP/1.0\r\n\r\n' % path
 
+    def handle_connect(self):
+        pass
+
+    def handle_close(self):
+        self.close()
+
+    def handle_read(self):
+        print self.recv(8192)
+
+    # def writable(self):
+        # return (len(self.buffer) > 0)
+
+    def handle_write(self):
+        pass
+        # self.buffer = self.buffer[sent:]
+
+
+
+class Client():
+    def __init__(self):
+        self.client = SocketClient('10.0.1.21', 4001)
+        self.thread =  threading.Thread(target=asyncore.loop,kwargs = {'timeout':1} )
+    def start(self):
+        """Start the listening service"""
+        # here I create an instance of the SMTP server, derived from  asyncore.dispatcher
+        
+        # and here I also start the asyncore loop, listening for SMTP connection, within a thread
+        # timeout parameter is important, otherwise code will block 30 seconds after the smtp channel has been closed
+        
+        self.thread.start()  
+
+    def stop(self):
+        """Stop listening now to port 25"""
+        # close the SMTPserver to ensure no channels connect to asyncore
+        self.client.close()
+        # now it is save to wait for the thread to finish, i.e. for asyncore.loop() to exit
+        self.thread.join()
+
+    def sendMessages(self, messages):
+        self.client.send(messages)
+
+x = Client()
 try:
-    # Connect to server and send data
-    sock.connect((HOST, PORT))
-    sock.sendall(data + "\n")
+   
+   x.start()
 
-    # Receive data from the server and shut down
-    received = sock.recv(1024)
+   while True:
+    n = raw_input("\n\nSend?: ")
+    x.sendMessages(n)
 finally:
-    sock.close()
-
-print "Sent:     {}".format(data)
-print "Received: {}".format(received)
+    x.stop()
--- host.py
+++ host.py
@@ -1,30 +1,85 @@
-import SocketServer
+import asyncore
+import socket
+import threading
 
-class MyTCPHandler(SocketServer.BaseRequestHandler):
-    """
-    The request handler class for our server.
+class SocketHandler(asyncore.dispatcher_with_send):
+    def __init__(self):
+        self.listeners = []
+    def handle_read(self):
+        data = self.recv(8192)
+        for listener in self.listeners:
+            listener.receice(data)
+        #     # self.send(data)
+    def sendMessages(self, messages):
+        self.send(messages)
 
-    It is instantiated once per connection to the server, and must
-    override the handle() method to implement communication to the
-    client.
-    """
+    def register(self, listener):
+        self.listeners.append(listener)
+        
 
-    def handle(self):
-        # self.request is the TCP socket connected to the client
-        self.data = self.request.recv(1024).strip()
-        print "{} wrote:".format(self.client_address[0])
-        print self.data
-        # just send back the same data, but upper-cased
-        self.request.sendall(self.data.upper())
+class MyServer(asyncore.dispatcher):
 
-def initServer():
-    HOST, PORT = "10.0.1.21", 4001
+    def __init__(self, host, port):
+        asyncore.dispatcher.__init__(self)
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.set_reuse_addr()
+        self.bind((host, port))
+        self.listen(5)
+        self.handlers = []
 
-    # Create the server, binding to localhost on port 9999
-    server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
+    def handle_accept(self):
+        pair = self.accept()
+        if pair is not None:
+            sock, addr = pair
+            print 'Incoming connection from %s' % repr(addr)
+            handler = SocketHandler(sock)
+            self.handlers.append(handler)
 
-    # Activate the server; this will keep running until you
-    # interrupt the program with Ctrl-C
-    server.serve_forever()
-    
-    
+    def sendMessages(self, messages):
+        for handler in self.handlers:
+            handler.sendMessages(messages)
+
+    def register(self, listener):
+        for handler in self.handlers:
+            handler.register(handler) 
+
+
+class Host():
+    def __init__(self):
+        self.server = MyServer('10.0.1.21', 4001)
+        self.thread =  threading.Thread(target=asyncore.loop,kwargs = {'timeout':1} )
+    def start(self):
+        """Start the listening service"""
+        # here I create an instance of the SMTP server, derived from  asyncore.dispatcher
+        
+        # and here I also start the asyncore loop, listening for SMTP connection, within a thread
+        # timeout parameter is important, otherwise code will block 30 seconds after the smtp channel has been closed
+        
+        self.thread.start()  
+
+    def stop(self):
+        """Stop listening now to port 25"""
+        # close the SMTPserver to ensure no channels connect to asyncore
+        self.server.close()
+        # now it is save to wait for the thread to finish, i.e. for asyncore.loop() to exit
+        self.thread.join()
+
+    def sendMessages(self, messages):
+        self.server.sendMessages(messages)
+
+    def register(self, listener):
+        self.server.register(listener)
+
+# x = Host()
+# try:
+   
+#    x.start()
+#    while True:
+#     n = raw_input("\n\nSend?: ")
+#     x.sendMessages(n)
+#     pass
+# finally:
+#     x.stop()
+ # h.sendMessages('123456')
+# 
+
